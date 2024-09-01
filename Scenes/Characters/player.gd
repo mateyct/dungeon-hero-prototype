af6c88f830d01395f0e77e5db816a47cffb728aa
mateyct <masonctolley1@gmail.com>
2024-08-31 21:12:01 -0600

    I got smooth movement working

--- Scenes/Characters/player.gd
+++ Scenes/Characters/player.gd
@@ -1,25 +1,22 @@
 extends CharacterBody3D
 
-var target_location: Vector3
-var starting_position: Vector3
-var is_mouse_pressed: bool = false
-
 func _physics_process(delta):
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
 	
-	if (Input.is_action_just_released("move_here")):
-		is_mouse_pressed = false
-	
-	if(target_location and (position - target_location).length() > .5):
-		velocity = (target_location - starting_position).normalized() * delta * 500
-		move_and_slide()
-	else:
-		velocity = Vector3(0, 0, 0)
+	if(direction != Vector3.ZERO):
+		var toLook : Basis = Basis.looking_at(direction.normalized())
+		basis = lerp(basis, toLook, .25)
 
-func _on_ground_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	if(event.is_action_pressed("move_here")):
-		is_mouse_pressed = true
-	if(is_mouse_pressed):
-		target_location = event_position
-		starting_position = position
 	
-		
+	velocity = direction.normalized()  * 14
+	
+	
+	move_and_slide()
